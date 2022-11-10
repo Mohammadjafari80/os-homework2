c00ad77c2d75da54373c6444bd720d61d673d570
Mohammad <mamad.jafari91@gmail.com>
2022-11-10 21:29:40 +0330

    add more error conditions

--- reverse.c
+++ reverse.c
@@ -8,7 +8,7 @@ int main(int args, char *argv[]) {
 	int counter = 0;
 	int f = open("file_to_read.txt", O_RDONLY);
 	if (f < 0){
-		write(1, "opening file failed!\n", 21);
+		write(2, "opening file failed!\n", 21);
 		return -1;
 	}
 	char* end = A;
@@ -17,7 +17,7 @@ int main(int args, char *argv[]) {
 		char temp;
 		int n = read(f, &temp, 1);
 		if (n < 0){
-			write(1, "reading file failed!\n", 21);
+			write(2, "reading file failed!\n", 21);
 			return -1;
 		}
 		if (temp=='\n'){
@@ -40,44 +40,62 @@ int main(int args, char *argv[]) {
 	pid_t child_pid = fork();
 
 	if (child_pid < 0){
-		write(1, "fork failed!\n", 13);
+		write(2, "fork failed!\n", 13);
 		return -1;
 	} else if (child_pid == 0) {
 		write(1, "child process started!\n", 23);
+
 		int f_reverse = open("file_to_write.txt", O_CREAT | O_WRONLY | O_TRUNC, 0642);
 		if (f_reverse < 0){
-			write(1, "opening file failed!\n", 21);
+			write(2, "opening file failed!\n", 21);
 			return -1;
 		}
-		write(f_reverse, B, counter+1);
+
+		int write_reverse = write(f_reverse, B, counter+1);
+		if (write_reverse < 0){
+			write(2, "writing file failed!\n", 21);
+			return -1;
+		}
+
 		write(1, "writed reverse!\n", 16);
+
 		int c_reverse = close(f_reverse);
 		if (c_reverse < 0){
-			write(1, "closing file failed!\n", 21);
+			write(2, "closing file failed!\n", 21);
 			return -1;
 		}
+
 		write(1, "child process ended!\n", 21);
 	} else if (child_pid > 0){
 		int status;
 		pid_t waited_pid = waitpid(child_pid, &status, 0);
 		
 		if (waited_pid < 0){
-			write(1, "waitpid() failed!\n", 18);
+			write(2, "waitpid() failed!\n", 18);
 			return -1;
 		} else if (waited_pid == child_pid) {
 			write(1, "parent process started!\n", 24); 
+
 			int f_normal = open("file_to_write.txt", O_APPEND | O_WRONLY, 0642);
 			if (f_normal < 0){
-				write(1, "opening file failed!\n", 21);
+				write(2, "opening file failed!\n", 21);
 				return -1;
 			}
-			write(f_normal, A, counter+1);
+			
+			int write_normal = write(f_normal, A, counter+1);
+			if (write_normal < 0){
+				write(2, "writing file failed!\n", 21);
+				return -1;
+			}
+
 			write(1, "writed normal!\n", 15);
+
 			int c_normal = close(f_normal);
 			if (c_normal < 0){
-				write(1, "closing file failed!\n", 21);
+				write(2, "closing file failed!\n", 21);
 				return -1;
 			}
+
 			write(1, "parent process ended!\n", 22);
 		}
 	}
